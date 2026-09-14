7eade0a5a88cc39fe3ba3d2fa91731012066f48a
Cristian Scheid <cristianscheid@gmail.com>
2026-09-14 14:50:26 -0300

    refactor: port RememberLoginToken to new entity system and use snowflake ids
    Signed-off-by: Cristian Scheid <cristianscheid@gmail.com>

--- core/Migrations/Version36000Date20260908184209.php
+++ core/Migrations/Version36000Date20260908184209.php
@@ -21,7 +21,7 @@ use Override;
 
 #[CreateTable(
 	table: 'remember_login_tokens',
-	columns: ['uid', 'token', 'created'],
+	columns: ['uid', 'token'],
 	description: 'New table to store remember login tokens, replacing the login_token entries kept in oc_preferences',
 )]
 #[AddIndex(table: 'remember_login_tokens', type: IndexType::PRIMARY)]
@@ -37,7 +37,6 @@ class Version36000Date20260908184209 extends SimpleMigrationStep {
 		if (!$schema->hasTable('remember_login_tokens')) {
 			$table = $schema->createTable('remember_login_tokens');
 			$table->addColumn('id', Types::BIGINT, [
-				'autoincrement' => true,
 				'notnull' => true,
 				'length' => 20,
 				'unsigned' => true,
@@ -50,14 +49,11 @@ class Version36000Date20260908184209 extends SimpleMigrationStep {
 				'notnull' => true,
 				'length' => 200,
 			]);
-			$table->addColumn('created', Types::BIGINT, [
-				'notnull' => true,
-				'length' => 20,
-				'unsigned' => true,
-			]);
 			$table->setPrimaryKey(['id']);
 			$table->addUniqueIndex(['token'], 'remember_login_tokens_token');
 			$table->addIndex(['uid'], 'remember_login_tokens_uid');
+			// Makes sure there is no auto-increment in Oracle
+			$schema->dropAutoincrementColumn('remember_login_tokens', 'id');
 
 			return $schema;
 		}
